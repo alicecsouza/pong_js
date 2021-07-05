7e997f5e7f9ee9afad4137fa684ae79247408715
alice souza <alicecsouza1@outlook.com>
2021-07-05 13:43:26 -0300

    primeiro commit

---
+++ imagens.js
@@ -0,0 +1,9 @@
+let imagemDaEstrada;
+let imagemDoAtor;
+let imagemCarro;
+
+function preload(){
+  imagemDaEstrada = loadImage("Imagens/estrada.png")
+  imagemDoAtor = loadImage("Imagens/ator-1.png")
+  imagemCarro = loadImage("Imagens/carro-1.png")
+}
